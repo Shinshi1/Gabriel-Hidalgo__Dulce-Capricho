01292417ad8cb449541565b6b7291bd841f211e0
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2022-05-02 00:17:31 -0300

    agregue bootstrap al proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
-const navToggle = document.querySelector(".nav__toggle")
-const navMenu = document.querySelector(".nav__menu")
+// Antiguo navToggle
+// const navToggle = document.querySelector(".nav__toggle")
+// const navMenu = document.querySelector(".nav__menu")
 
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav__menu--visible")
-    });
+// navToggle.addEventListener("click", () => {
+    // navMenu.classList.toggle("nav__menu--visible")
+    // });
