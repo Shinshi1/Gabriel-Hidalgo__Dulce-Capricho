41cdc476e6914a5978788108f42744c6ff596efc
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2022-05-15 21:28:54 -0300

    se agregan img de productos, se indenta y comenta prolijamente el html, se corrigen errores del carousel

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-// Antiguo navToggle
-// const navToggle = document.querySelector(".nav__toggle")
-// const navMenu = document.querySelector(".nav__menu")
-
-// navToggle.addEventListener("click", () => {
-    // navMenu.classList.toggle("nav__menu--visible")
-    // });
